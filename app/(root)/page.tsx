5944d7a30aa89a39a1fd1915b70e499b8da00ff2
Wali Ullah <wali190939@gmail.com>
2024-01-19 02:07:22 +0600

    update root

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,17 +5,18 @@ import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { useEffect } from "react";
 const SetupPage = () => {
-  const onOpen=useStoreModal((state)=>state.onOpen);
-  const isOpen=useStoreModal((state)=>state.isOpen);
-  useEffect(()=>{
-    if(!isOpen){
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+  useEffect(() => {
+    if (!isOpen) {
       onOpen();
     }
-  },[isOpen,onOpen]);
-  
+  }, [isOpen, onOpen]);
+
   return (
     <>
       <p>hello world</p>
+      <p>test </p>
     </>
   );
 };

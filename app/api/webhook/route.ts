3fd55e9924584c27c02e27a4fadb1dcc74201da3
Wali Ullah <wali190939@gmail.com>
2024-02-06 18:18:05 +0600

    fix: typo

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
       data: {
         isPaid: true,
         address: addressString,
-        phone: session?.customer_details?.phone,
+        phone: session?.customer_details?.phone || "",
       },
       include: {
         orderItems: true,

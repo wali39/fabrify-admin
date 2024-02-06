57aa24d9763e902eed19a26b7e131f16d8b7d6cf
Wali Ullah <wali190939@gmail.com>
2024-02-06 18:05:38 +0600

    update db aiven and webhook api

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,13 +1,16 @@
 import Stripe from "stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+
 import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
 export async function POST(req: Request) {
   const body = await req.text();
   const signature = headers().get("Stripe-Signature") as string;
+
   let event: Stripe.Event;
+
   try {
     event = stripe.webhooks.constructEvent(
       body,
@@ -15,23 +18,24 @@ export async function POST(req: Request) {
       process.env.STRIPE_WEBHOOK_SECRET!
     );
   } catch (error: any) {
-    return new NextResponse(`Webhook error : ${error.message}`, {
-      status: 400,
-    });
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
   }
 
   const session = event.data.object as Stripe.Checkout.Session;
   const address = session?.customer_details?.address;
+  console.debug("webhook", address);
   const addressComponents = [
     address?.line1,
     address?.line2,
     address?.city,
+    address?.state,
     address?.postal_code,
+    address?.country,
   ];
 
-  const addressString = addressComponents.filter((c) => c !== null).join(",");
+  const addressString = addressComponents.filter((c) => c !== null).join(", ");
 
-  if (event.type == "checkout.session.completed") {
+  if (event.type === "checkout.session.completed") {
     const order = await prismadb.order.update({
       where: {
         id: session?.metadata?.orderId,
@@ -39,13 +43,15 @@ export async function POST(req: Request) {
       data: {
         isPaid: true,
         address: addressString,
-        phone: session?.customer_details?.phone || "",
+        phone: session?.customer_details?.phone,
       },
       include: {
         orderItems: true,
       },
     });
-    const productIds = order.orderItems.map((item) => item.productId);
+
+    const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
     await prismadb.product.updateMany({
       where: {
         id: {
@@ -57,5 +63,6 @@ export async function POST(req: Request) {
       },
     });
   }
+
   return new NextResponse(null, { status: 200 });
 }
